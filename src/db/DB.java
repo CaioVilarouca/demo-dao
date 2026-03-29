d865522e9090e1d10f8252c4ed77dda35fae25f8
Caio <caiovilarouca@gmail.com>
2026-03-28 21:24:55 -0300

    feat

--- src/db/DB.java
+++ src/db/DB.java
@@ -2,9 +2,26 @@ package db;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 public class DB {
+	private static Connection connection = null;
+	private static Connection getConnection() {
+		if (connection == null) {
+			try {
+				Properties properties = loadProperties();
+				String url = properties.getProperty("url");
+				connection = DriverManager.getConnection(url, properties);
+			} 
+			catch (SQLException e) {
+				throw new DbException("Não foi possivel fazer a conexão com o banco. Erro: "+e.getMessage());
+			}
+		}
+		return connection;
+	}
 	private static Properties loadProperties() { // Carregar
 		try (FileInputStream fs = new FileInputStream("db.properties")) {
 			Properties properties = new Properties();
@@ -12,7 +29,7 @@ public class DB {
 			return properties;
 		}
 		catch (IOException e ) {
-			throw new DbException("Não foi possivel carregar o arquivo. "+e.getMessage());
+			throw new DbException("Não foi possivel carregar o arquivo. Erro: "+e.getMessage());
 		}
 	}
 }
--- src/module-info.java
+++ src/module-info.java
@@ -5,4 +5,5 @@
  * 
  */
 module demo_dao {
+	requires java.sql;
 }
